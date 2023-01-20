293e771ae1b0d6d64ae1a963bbaf43b2850d4a10
zoirj0n <zoirbek.sobirov@mail.ru>
2023-01-20 23:42:02 +0400

    updated

---
+++ philosophers/philo/check_done_eat_and_die.c
@@ -0,0 +1,65 @@
+#include "philo.h"
+
+void	death(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->info->die));
+	philo->info->died = 1;
+	pthread_mutex_unlock(&(philo->info->die));
+}
+
+void	done_eat(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->info->die));
+	philo->info->done_eat = 1;
+	pthread_mutex_unlock(&(philo->info->die));
+}
+
+int	ft_check_died(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->info->die));
+	if (philo->info->died)
+	{
+		pthread_mutex_unlock(&(philo->info->die));
+		return (1);
+	}
+	pthread_mutex_unlock(&(philo->info->die));
+	return (0);
+}
+
+int	check_death(t_philo *philo)
+{
+	int			i;
+	long long	time;
+
+	pthread_mutex_lock(&philo->info->count_eat);
+	if (philo->info->must_eat > 0 && \
+		philo->info->count >= philo->info->num_philo)
+		done_eat(philo);
+	pthread_mutex_unlock(&philo->info->count_eat);
+	i = 0;
+	while (i < philo->info->num_philo)
+	{
+		pthread_mutex_lock(&(philo->info->eating));
+		time = philo[i].last_ate;
+		pthread_mutex_unlock(&(philo->info->eating));
+		if (get_time() - time >= philo->info->time_die)
+		{
+			print(philo, i, "died");
+			death(philo);
+		}
+		if (ft_check_died(philo))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	check_done_eat_and_die(t_philo *phil)
+{
+	while (!phil->info->done_eat)
+	{
+		usleep(200);
+		if (check_death(phil) || phil->info->done_eat)
+			break ;
+	}
+}
---
+++ philosophers/philo/philo.c
@@ -0,0 +1,51 @@
+#include "philo.h"
+//argumentlarining hammasi raqam ekanligini tekshirish uchun 
+int	check(int ac, char **av)
+{
+	int			i;
+	int			j;
+
+	i = 1;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (!ft_isdigit(av[i][j]))
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	main(int ac, char **av)
+{
+	t_philo		philo;
+	t_info		info;
+
+	if (ac > 6 || ac < 5)
+	{
+		printf("Invalid arguments\n");
+		return (0);
+	}
+	if (check(ac, av) == 0)
+	{
+		printf("Error found letter in arguments\n");
+		return (0);
+	}
+	if (double_check(ac, av) == 0)
+	{
+		printf("Invalid arguments\n");
+		return (0);
+	}
+	info_init(&philo, &info, ac, av);
+	return (0);
+}
+//we are stocking the argument, creating 
+//variable that we will use after as indicator.
+//creating the mutex
+//crating a structur that each philo created will have
+//creating the thread
+//and free all the malloc
