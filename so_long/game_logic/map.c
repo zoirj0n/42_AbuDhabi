585b3cf241c7020e470c4f88f4be728532b452c7
zoirj0n <zosobiro@student.42abudhabi.ae>
2023-01-25 13:38:18 +0400

    added extension validator

--- so_long/game_logic/map.c
+++ so_long/game_logic/map.c
@@ -6,7 +6,7 @@
 /*   By: zosobiro <zosobiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 11:39:02 by zosobiro          #+#    #+#             */
-/*   Updated: 2023/01/15 15:27:07 by zosobiro         ###   ########.fr       */
+/*   Updated: 2023/01/25 13:09:44 by zosobiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,43 @@ static int	add_line(t_complete *game, char *line)
 	return (1);
 }
 
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		++i;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+int	ft_check_file_extension(char *file)
+{
+	char	*extension;
+	char	*right_extension;
+	int		file_name_length;
+	int		i;
+
+	file_name_length = 0;
+	while (file[file_name_length] != '\0')
+		file_name_length++;
+	file_name_length = file_name_length - 4;
+	right_extension = ".ber";
+	extension = malloc(sizeof(char) * 4);
+	i = 0;
+	while (file[file_name_length] != '\0' && i < 4)
+	{
+		extension[i] = file[file_name_length];
+		file_name_length++;
+		i++;
+	}
+	extension[i] = '\0';
+	if (ft_strcmp(extension, right_extension) == 0)
+		return (1);
+	else
+		return (0);
+}
+
 int	map_reading(t_complete *game, char **argv)
 {
 	char	*readmap;
@@ -54,6 +91,11 @@ int	map_reading(t_complete *game, char **argv)
 	game->fd = open(argv[1], O_RDONLY);
 	if (game->fd < 0)
 		return (0);
+	if (!ft_check_file_extension(argv[1]))
+	{
+		printf("\e[31m\e[1mError\n File extension is not Valid\n");
+		exit_point(game);
+	}
 	while (1)
 	{
 		readmap = get_next_line(game->fd);
