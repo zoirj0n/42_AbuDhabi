6bfaf78b8122435d9df05b8b36df1d811c77d446
zoirj0n <zosobiro@student.42abudhabi.ae>
2023-02-02 00:08:14 +0400

    updated-V_9

--- mini_talk/minitalk.h
+++ mini_talk/minitalk.h
@@ -6,21 +6,21 @@
 /*   By: zosobiro <zosobiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 16:59:58 by zosobiro          #+#    #+#             */
-/*   Updated: 2023/01/27 18:45:44 by zosobiro         ###   ########.fr       */
+/*   Updated: 2023/02/02 00:06:56 by zosobiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
-#define MINITALK_H
+# define MINITALK_H
 
-#include <sys/types.h>
-#include <signal.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+# include <sys/types.h>
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
-void ft_putchar_fd(char c, int fd);
-void ft_putnbr_fd(int n, int fd);
-int ft_atoi(const char *str);
-void ft_error(const char *str, int flag);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+int		ft_atoi(const char *str);
+void	ft_error(const char *str, int flag);
 #endif
