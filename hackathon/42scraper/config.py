785ba5731e8e31473d5440f7bae866cb0eac5a2a
Zoirjon <45711236+zoirj0n@users.noreply.github.com>
2023-02-08 13:31:54 +0400

    Update config.py

--- hackathon/42scraper/config.py
+++ hackathon/42scraper/config.py
@@ -1,4 +1,4 @@
-uid="u-s4t2ud-da794d8b0c77ed8311ec73a40b2ae46589bd1903a2aedba7bf5e07dd9f247590"
-secret_code="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"
+uid="YOUR UID"
+secret_code="YOUR SECRET CODE"
 username="YOUR USERNAME"
 password="YOUR PASSWORD"
