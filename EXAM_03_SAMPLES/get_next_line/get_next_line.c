ad4d937921e5018a3ec935f723f60311438f9143
zoirj0n <zosobiro@student.42abudhabi.ae>
2023-02-09 12:36:53 +0400

    updated_2.3

--- EXAM_03_SAMPLES/get_next_line/get_next_line.c
+++ EXAM_03_SAMPLES/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: zosobiro <zosobiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 15:29:51 by zosobiro          #+#    #+#             */
-/*   Updated: 2023/02/07 15:30:11 by zosobiro         ###   ########.fr       */
+/*   Updated: 2023/02/08 15:02:17 by zosobiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,37 @@ char	*get_next_line(int fd)
 	while (read(fd, buffer, 1) > 0 && *buffer++ != '\n');
 	return (buffer > string) ? (*buffer = 0, string) : (free(string), NULL);
 }
+
+// #include <unistd.h>
+// #include <stdlib.h>
+
+// char	*get_next_line(int fd)
+// {
+// 	int		rd = 0;
+// 	char	c;
+// 	char	*line = malloc(100000);
+// 	char	*word = line;
+// 	if ((rd = read(fd, &c, 1)) == 0)
+// 		return (NULL);
+// 	*line++ = c;
+// 	while ((rd = read(fd, &c, 1)) > 0 && c != '\n')
+// 		*line++ = c;
+// 	if (c == '\n')
+// 		*line++ = '\n';
+// 	*line = '\0';
+// 	return (word);
+// }
+
+// #include <stdio.h>
+// int main() // main1.c
+// {
+// 	char *line;
+// 	line = get_next_line(0);
+// 	while (line != NULL)
+// 	{
+// 		printf("%s", line);
+// 		free (line);
+// 		line = get_next_line(0);
+// 	}
+// 	return (0);
+// }
