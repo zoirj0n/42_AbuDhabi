98f873abd3727db45ef3fe02b4473e05203872bd
Zoirjon <45711236+zoirj0n@users.noreply.github.com>
2023-02-16 10:46:02 +0400

    Update get_next_line.c

--- EXAM_03_SAMPLES/get_next_line/get_next_line.c
+++ EXAM_03_SAMPLES/get_next_line/get_next_line.c
@@ -10,45 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
-	char	*string = malloc(10000), *buffer = string;
-	while (read(fd, buffer, 1) > 0 && *buffer++ != '\n');
-	return (buffer > string) ? (*buffer = 0, string) : (free(string), NULL);
+    int     i = 0;
+    int     rd = 0;
+    char    character;
+    char     *buffer = malloc(10000);
+
+    while ((rd = read(fd, &character, BUFFER_SIZE - BUFFER_SIZE + 1)) > 0)
+    {
+        buffer[i++] = character;
+        if (character == '\n')
+            break;
+    }
+    if ((!buffer[i - 1] && !rd) || rd == -1)
+    {
+        free(buffer);
+        return (NULL);
+    }
+    buffer[i] =  '\0';
+    return(buffer);
 }
-
-// #include <unistd.h>
-// #include <stdlib.h>
-
-// char	*get_next_line(int fd)
-// {
-// 	int		rd = 0;
-// 	char	c;
-// 	char	*line = malloc(100000);
-// 	char	*word = line;
-// 	if ((rd = read(fd, &c, 1)) == 0)
-// 		return (NULL);
-// 	*line++ = c;
-// 	while ((rd = read(fd, &c, 1)) > 0 && c != '\n')
-// 		*line++ = c;
-// 	if (c == '\n')
-// 		*line++ = '\n';
-// 	*line = '\0';
-// 	return (word);
-// }
-
-// #include <stdio.h>
-// int main() // main1.c
-// {
-// 	char *line;
-// 	line = get_next_line(0);
-// 	while (line != NULL)
-// 	{
-// 		printf("%s", line);
-// 		free (line);
-// 		line = get_next_line(0);
-// 	}
-// 	return (0);
-// }
