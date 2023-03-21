7a43bdbd86f62a461cb65e7a8069154b92c41a6b
zoirj0n <zosobiro@student.42abudhabi.ae>
2023-03-21 12:06:55 +0400

    updated

--- CPP/module_01/ex00/newZombie.cpp
+++ CPP/module_01/ex00/newZombie.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zosobiro <zosobiro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 02:29:23 by zosobiro          #+#    #+#             */
+/*   Updated: 2023/03/15 02:29:24 by zosobiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Zombie.hpp"
 
 // It creates a zombie, name it, and return it so you can use it outside of the function scope.
