e1d8802b735d35304847a33ce9dab7d1580dda16
zoirj0n <zosobiro@student.42abudhabi.ae>
2023-03-23 14:31:28 +0400

    updated

--- CPP/module_03/ex00/ClapTrap.cpp
+++ CPP/module_03/ex00/ClapTrap.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zosobiro <zosobiro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 03:48:25 by zosobiro          #+#    #+#             */
+/*   Updated: 2023/03/16 03:48:26 by zosobiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap( std::string name ) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
